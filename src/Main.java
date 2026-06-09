c5b03c7c44f25b4d4e3d67b0e234b74c4ddb8ecd
slark <Slark.mk21@gmail.com>
2026-06-09 12:23:12 +0300

    Q2

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@
 // нажмите на значок <icon src="AllIcons.Actions.Execute"/> на полях.
 public class Main {
     public static void main(String[] args) {
-    var dog = 8;
-    var cat = 3.6;
-    var paper = 76378;
+    var dog = 8 + 4 ;
+    var cat = 3.6 + 4;
+    var paper = 76378 + 4;
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
