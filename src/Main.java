217a1a0ba471db5ad8cef9d7c6e5ecac9c508aad
slark <Slark.mk21@gmail.com>
2026-06-11 15:05:23 +0300

    Hm2

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
         //Q1
 
-        var dog = 8;
+        var dog = 8.0;
         var cat = 3.6;
         var paper = 76378;
         System.out.println(dog);
@@ -14,18 +14,18 @@ public class Main {
 
         //Q2
 
-        var dog = 8 + 4 ;
-        var cat = 3.6 + 4;
-        var paper = 76378 + 4;
+        dog = dog + 4 ;
+        cat = cat + 4;
+        paper = paper + 4;
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
 
         //Q3
 
-        var dog = 8 - 3.5 ;
-        var cat = 3.6 - 1.6;
-        var paper = 76378 - 7639;
+        dog = dog - 3.5;
+        cat = cat - 1.6;
+        paper = paper - 7639;
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
@@ -34,16 +34,21 @@ public class Main {
 
         var friend = 19;
         System.out.println(friend);
-        System.out.println(friend + 2 );
-        System.out.println(friend / 7 );
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
 
         //Q5
 
         var frog = 3.5;
         System.out.println(frog);
-        System.out.println(frog + 10 );
-        System.out.println(frog / 3.5 );
-        System.out.println(frog / 3.5 + 4);
+        frog = frog + 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
 
         //Q6
 
@@ -54,8 +59,8 @@ public class Main {
 
         //Q7
 
-        var boxer1 = 78.2;
-        var boxer2 = 82.7;
+        boxer1 = 78.2;
+        boxer2 = 82.7;
         var result = boxer2 % boxer1;
         System.out.println("Остаток от деления boxer2 на boxer1 = " + result);
 
@@ -66,7 +71,7 @@ public class Main {
         var people = workTime / peopleWork;
         var newWork = 94;
         System.out.println("Всего работников в компании — " + people + " человек");
-        System.out.println("Если в компании работает " + (people + newWork) + " человек, то всего " + workTime / (people + newWork)  + " часов работы может быть поделено между сотрудниками");
+        System.out.println("Если в компании работает " + (people + newWork) + " человек, то всего " + (people + newWork) * peopleWork  + " часов работы может быть поделено между сотрудниками");
 
 
 
